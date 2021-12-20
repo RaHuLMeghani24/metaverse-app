a34c8806c70d3271495c05a5a853f1cc557c4f4e
Rahul Meghani <rahulmeghani36@gmail.com>
2021-12-20 17:35:35 -0500

    Finished with the login functionality

---
+++ components/Login.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Image from 'next/image';
+import {useMoralis} from "react-moralis";
+
+function Login() {
+
+    const {authenticate} = useMoralis();
+
+    
+    return (
+        <div className='bg-black relative'>
+           
+            <div className='flex flex-col absolute z-50 h-4/6 items-center justify-center w-full
+                            space-y-4'>
+                {/* Logo */}
+                <Image 
+                    className='object-cover rounded-full'
+                     src="https://links.papareact.com/3pi" height={200} width={200}
+                />
+
+                {/* Login button */}
+                <button onClick={authenticate} className='bg-yellow-500 rounded-lg p-5 font-bold animate-pulse'>
+                    Login to the MetaVerse
+                </button>
+            </div>
+ 
+            <div className='w-full h-screen'>
+                {/* Login form */}      
+                <Image 
+                        src="https://links.papareact.com/55n"
+                        layout='fill'
+                        objectFit='cover'
+                />
+            </div>
+        </div>
+    )
+}
+
+export default Login
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,21 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import Login from "../components/Login";
+import {useMoralis} from "react-moralis";
 
 export default function Home() {
+
+  const {isAuthenticated, logout} = useMoralis();
+
+    if(!isAuthenticated) return <Login/>
   return (
-    <div className="">
+    <div className="h-screen">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
     <h1>Welcome to the Challenge</h1>
+
+    <button onClick={logout}>Logout</button>
       
     </div>
   )
